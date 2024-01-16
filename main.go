0680286842c327a397ba00070133405e71d6c16c
Kunal Singh <kunalsin9h@gmail.com>
2024-01-17 00:46:25 +0530

    doing something

--- main.go
+++ main.go
@@ -1,45 +1,15 @@
 package main
 
 import (
-	"context"
-	"fullstackgo/view"
-	"fullstackgo/view/layout"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"fullstackgo/zinc"
+	"log/slog"
 )
 
-type GlobalState struct {
-	Count int
-}
-
-var global GlobalState
-
 func main() {
-	echoServer := echo.New()
-	echoServer.HideBanner = true
+	app := zinc.New()
 
-	echoServer.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"https://i.imgur.com"},
-		AllowMethods: []string{"GET"},
-	}))
-
-	echoServer.Static("/static/", "./static")
-
-	echoServer.GET("/", func(c echo.Context) error {
-		ctx := c.Request().Context()
-		ctx = context.WithValue(ctx, "host", c.Request().Host)
-
-		return view.Index(global.Count).Render(ctx, c.Response().Writer)
-	})
-
-	echoServer.POST("/", func(c echo.Context) error {
-		ctx := c.Request().Context()
-
-		global.Count += 1
-
-		return layout.CountsButton(global.Count).Render(ctx, c.Response().Writer)
-	})
-
-	echoServer.Logger.Fatal(echoServer.Start("127.0.0.1:3000"))
+	err := app.Start()
+	if err != nil {
+		slog.Error(err.Error())
+	}
 }
---
+++ zinc/zinc.go
@@ -0,0 +1,108 @@
+package zinc
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"net"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"sync/atomic"
+
+	"github.com/labstack/echo/v4"
+)
+
+type App struct {
+	Server        *echo.Echo
+	Addr          string
+	TotalHandlers uint32
+	Context       context.Context
+}
+
+func New() *App {
+	server := echo.New()
+	server.HideBanner = true
+	server.HidePort = true
+
+	return &App{
+		Server:  server,
+		Context: context.Background(),
+	}
+}
+
+func (a *App) registerEndpoints() error {
+	slog.Info("Registering Handlers...")
+	const sourcePath = "./src/pages/"
+	const sourcePrefix = "src/pages"
+
+	totalHandlers := uint32(0)
+
+	err := filepath.Walk(sourcePath, func(path string, info fs.FileInfo, err error) error {
+		// only file who are generated
+		codeFileName, found := strings.CutSuffix(path, "_templ.go")
+		if found {
+			// remove sourcePath
+			endpoint, _ := strings.CutPrefix(codeFileName, sourcePrefix)
+			if endpoint == "/index" {
+				endpoint = "/"
+			}
+
+			// Increasing the count of  total endpoints registered
+			// TODO: read the function docs, they say to do something else  which is less error prone
+			atomic.AddUint32(&totalHandlers, 1)
+
+			a.Server.GET(endpoint, func(c echo.Context) error {
+				// (:0 See path is from line filepath.Walk function above
+				html := a.getHTML(path)
+
+				return c.String(http.StatusOK, html)
+			})
+		}
+		return nil
+	})
+
+	a.TotalHandlers = totalHandlers
+	return err
+}
+
+func (a *App) getHTML(filePath string) string {
+	return filePath
+}
+
+func (a *App) setAddr(baseURL ...string) {
+	var url string
+	if len(baseURL) == 0 {
+		url = ""
+	} else {
+		url = baseURL[0]
+	}
+
+	host, port, err := net.SplitHostPort(url)
+	if err != nil {
+		slog.Error(err.Error())
+
+		host, port = "127.0.0.1", "7156"
+		slog.Info("Using default host and port", "host", host, "port", port)
+		if ip := net.ParseIP(strings.Trim(url, "[]")); ip != nil {
+			host = ip.String()
+		}
+	}
+
+	a.Addr = net.JoinHostPort(host, port)
+}
+
+func (a *App) Start(baseURL ...string) error {
+	a.setAddr(baseURL...)
+
+	err := a.registerEndpoints()
+	if err != nil {
+		return err
+	}
+
+	slog.Info(fmt.Sprintf("Total Handlers registered: %d", a.TotalHandlers))
+	slog.Info(fmt.Sprintf("Listening at: %s\n", a.Addr))
+
+	return a.Server.Start(a.Addr)
+}
